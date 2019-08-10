bfb070a34bec1a259a9adfc988cc5a5a7793ef22
sidazhong <sida9567@gmail.com>
2019-08-09 21:08:11 -0700

    s

--- problem/mid_lin/406.php
+++ problem/mid_lin/406.php
@@ -19,7 +19,7 @@ class debug {
 	}
 }
 
-//核心思想：  找到靠近的数组后，后指针为前指针，前指针+1
+//核心思想：  找到超出范围的数字后，后指针为前指针，前指针+1
 class problem_406{
 	function __construct($array,$s) {
 		//给出的数组
@@ -31,6 +31,9 @@ class problem_406{
 		//记录指针
 		$this->pool=[];
 		
+		//记录长度
+		$this->length=[];
+		
 		//给出的最大值
 		$this->s=$s;
 		
@@ -39,42 +42,61 @@ class problem_406{
 		
 		//前指针
 		$this->f_index=0;
+		
+		//最短数组
+		$this->min=[];
 	}
 	
 	function start(){
 		foreach($this->array as $k=>$v){
 			//在滑动窗口范围内
 			if($this->window[$this->b_index]+$v<$this->s){
+				//重新窗口
 				$this->window[$this->b_index]+=$v;
 				
-				//前指针+1
-				$this->f_index++;
+				//记录指针
+				$this->pool[$this->b_index]=[$this->b_index,$this->f_index];
 			}
 			//超出窗口
 			else{
-				//记录指针
-				$this->pool[$this->b_index]=[$this->b_index,$this->f_index-1];
+				//记录长度
+				$this->length[$this->b_index]=$this->f_index-$this->b_index;
 				
 				//后指针为前指针
 				$this->b_index=$this->f_index-1;
 				
-				//前指针+1
-				$this->f_index++;
-				
 				//重新窗口
 				$this->window[$this->b_index]=$this->array[$this->b_index]+$v;
+				
+				//记录指针
+				$this->pool[$this->b_index]=[$this->b_index,$this->f_index];
 			}
+			
+			//前指针+1
+			$this->f_index++;
 		}
 		
 		//获取最大值的key
-		$max=array_keys($this->window, max($this->window));
-		return $this->pool[$max[0]];
+		$max_key = array_keys($this->window, max($this->window));
+		
+		//选出最短的数字
+		foreach($max_key as $v){
+			$this->min[$v]=count($this->array[$v]);
+		}
+		$min_key = array_keys($this->min, min($this->min));
+		
+		//找出对应数组元素集合
+		foreach($this->pool[$min_key[0]] as $v){
+			$rs[]=$this->array[$v];
+		}
+		
+		return $rs;
 	}
 	
 }
 
 //一个数组中，找出“最短”连续数组 最靠近一个数字
-$array=[1,3,1,2,5,6,2];		//[5,6]
+$array=[1,3,1,2,5,6,5];		//[5,6]
 $s=11;
 
 $obj=new problem_406($array,$s);
