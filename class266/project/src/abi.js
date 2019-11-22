2658eef03b15489ec810c15956bc8a3526224bd4
sidazhong <sida9567@gmail.com>
2019-11-21 17:56:00 -0800

    sida

---
+++ class266/project/src/abi.js
@@ -0,0 +1,32 @@
+var abi = [
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "getMessage",
+    "outputs": [
+      {
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function",
+    "signature": "0xce6d41de"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "newMessage",
+        "type": "string"
+      }
+    ],
+    "name": "setMessage",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function",
+    "signature": "0x368b8772"
+  }
+];
