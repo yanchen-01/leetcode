db8451e4b98008e78deed80b25a49a06d7699011
sidazhong <sida9567@gmail.com>
2020-02-01 18:55:14 -0800

    sida

---
+++ class265/bin/src/main/java/edu/sjsu/crypto/ciphersys/classic/test.java
@@ -0,0 +1,14 @@
+package class265.bin.src.main.java.edu.sjsu.crypto.ciphersys.classic;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class test {
+
+	@Test
+	void test() {
+		HelloWorld.greeting();
+	}
+
+}
