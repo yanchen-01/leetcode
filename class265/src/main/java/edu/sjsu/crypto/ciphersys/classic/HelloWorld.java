eb164d2ebfac1fc55b312f79e3052659d3df5906
sidazhong <sida9567@gmail.com>
2020-02-01 19:25:53 -0800

    sida

---
+++ class265/src/main/java/edu/sjsu/crypto/ciphersys/classic/HelloWorld.java
@@ -0,0 +1,17 @@
+package edu.sjsu.crypto.ciphersys.classic;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * This is a Hello Word class for Cipher Systems project. 
+ *  
+ * @author ahmad
+ */
+@Slf4j
+public class HelloWorld {
+  
+  public static void greeting() {
+    log.info("Hello World!");
+  }
+  
+}
